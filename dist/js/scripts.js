93c590f7de8b1d09becdcca1e46be2a522d27c3d
Ivan <ivan.frontcoder@gmail.com>
2022-01-13 15:45:47 +0500

    .

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -744,22 +744,22 @@ $(document).ready(function () {
 	// }
 	// JQuerySlider();
 
-	// // Смена положения блока при изменении ширины окна
-	// // function(блок, куда переместить, куда вернуть)
-	// function replace(block, to, from, mediaBreak) {
-	// 	function replaceToggle() {
-	// 		if ($(window).width() <= mediaBreak) { // условие на ширину окна
-	// 			block.appendTo(to); // Переместить блок
-	// 		} else {
-	// 			block.appendTo(from); // Вернуть блок обратно
-	// 		}
-	// 	}
-	// 	replaceToggle();
-	// 	$(window).resize(function () {
-	// 		replaceToggle();
-	// 	})
+	// Смена положения блока при изменении ширины окна
+	// function(блок, куда переместить, куда вернуть)
+	function replace(block, to, from, mediaBreak) {
+		function replaceToggle() {
+			if ($(window).width() <= mediaBreak) { // условие на ширину окна
+				block.appendTo(to); // Переместить блок
+			} else {
+				block.appendTo(from); // Вернуть блок обратно
+			}
+		}
+		replaceToggle();
+		$(window).resize(function () {
+			replaceToggle();
+		})
 
-	// }
-	// replace($('#block'), $('#to'), $('#from'), breakLg);
+	}
+	replace($('#blockReplace'), $('#to'), $('#from'), breakSm);
 
 });
